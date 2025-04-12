053d4de9a1a6c7981ce0aa166975fb57647e7520
AreebaMughal <124130777+AreebaMughal@users.noreply.github.com>
2025-04-12 22:27:43 +0500

    Update admin.php

--- admin.php
+++ admin.php
@@ -21,7 +21,7 @@ $result = $conn->query("SELECT * FROM orders ORDER BY order_time DESC");
       <h1 style="margin: 0; font-size: 1.8rem;">Beans&Brew</h1>
     </div>
     <nav class="nav-links">
-      <a href="homepage.php" style="text-decoration: none; font-size: larger; color: black; padding: 10px;">Home</a>
+      <a href="index.php" style="text-decoration: none; font-size: larger; color: black; padding: 10px;">Home</a>
       <a href="menu.php" style="text-decoration: none; font-size: larger; color: black; padding: 10px;">Menu</a>
       <a href="contact.php" style="text-decoration: none; font-size: larger; color: black; padding: 10px;">Contact Us</a>
       <a href="admin.php" style="text-decoration: none; font-size: larger; color: black; padding: 10px;">Admin</a>
